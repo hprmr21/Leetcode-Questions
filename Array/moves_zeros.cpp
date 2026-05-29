726effef8cb609e5327f15c1b746b10d109beddd
hprmr21 <harshpbrc@gmail.com>
2026-05-29 14:18:30 +0530

    Leetcode-Question

---
+++ Array/moves_zeros.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+      int n=nums.size();
+    //   vector<int>temp;
+    //   for(int i=0;i<n;i++){
+    //     if(nums[i]!=0){
+    //         temp.push_back(nums[i]);
+    //     }
+    //   }
+    //   for(int i=0;i<temp.size();i++){
+    //     nums[i]=temp[i];
+
+    //   }  
+    //   for(int i=temp.size();i<n;i++){
+    //     nums[i]=0;
+    //   }
+
+
+    int j=-1;
+    for(int i=0;i<n;i++){
+        if(nums[i]==0){
+            j=i;
+            break;
+        }
+    }
+    if(j==-1) return;
+    for(int i=j+1;i<n;i++){
+        if(nums[i]!=0){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            j++;
+
+        }
+    }
+
+    }
+};
