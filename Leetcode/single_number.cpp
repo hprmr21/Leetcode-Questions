ad1934d07ba21fd10e00471bf1f5547742b88fd8
hprmr21 <harshpbrc@gmail.com>
2026-06-02 13:27:52 +0530

    Leetcode-Question

---
+++ Leetcode/single_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+      
+      
+      
+      
+        int Xor=0;
+        for(int i=0;i<nums.size();i++){
+            Xor^=nums[i];
+        }
+        return Xor;
+    }
+};
