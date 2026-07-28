7b58a87699a0466f98d67d7859ec99560f8a3532
HARSH PARMAR <harshpbrc@gmail.com>
2026-07-28 19:38:18 +0530

    Time: 16 ms (5.73%), Space: 124.3 MB (32.88%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,19 +10,47 @@
  */
 class Solution {
 public:
+    ListNode* reverseLL(ListNode* head){
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* newHead = reverseLL(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = nullptr;
+        return newHead;
+    }
+
     bool isPalindrome(ListNode* head) {
-        stack<int>st;
-        ListNode* temp = head;
-        while(temp != NULL){
-            st.push(temp->val);
-            temp = temp->next;
+        // stack<int>st;
+        // ListNode* temp = head;
+        // while(temp != NULL){
+        //     st.push(temp->val);
+        //     temp = temp->next;
+        // }
+        // temp = head;
+        // while(temp != NULL){
+        //     if(temp->val != st.top())return false;
+        //     temp = temp->next;
+        //     st.pop();
+        // }
+        // return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        temp = head;
-        while(temp != NULL){
-            if(temp->val != st.top())return false;
-            temp = temp->next;
-            st.pop();
+        ListNode* newHead = reverseLL(slow->next);
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while(second != NULL){
+            if(first->val != second->val){
+                reverseLL(newHead);
+                return false;
+            }
+            first = first->next;
+            second = second->next;
         }
+        reverseLL(newHead);
         return true;
     }
 };
